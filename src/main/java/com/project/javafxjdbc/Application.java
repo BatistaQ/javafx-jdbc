9d7418153cc63ac769abd54d56f1291cee5adf1a
BatistaQ <kauanbs29@hotmail.com>
2024-12-31 11:45:27 -0300

    application view

--- src/main/java/com/project/javafxjdbc/Application.java
+++ src/main/java/com/project/javafxjdbc/Application.java
@@ -8,12 +8,16 @@ import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800,600);
+            stage.setScene(scene);
+            stage.setTitle("Sample JavaFX application");
+            stage.show();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
